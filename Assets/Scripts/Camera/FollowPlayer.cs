d2852c63b996e04e641bb5477a1bb94d3ff97b80
Aileen Denton <ajdenton96@gmail.com>
2020-12-19 20:07:01 +0000

    Character Movement
    Movement applied through inputs for horizontal movement. Camera scripted to follow position of player.

---
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // Defining a game object which will hold the player object.
+    public GameObject player;
+
+    // Vector3 variable for offsetting the camera
+    private Vector3 cameraOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Setting the cameraOffset
+        cameraOffset = new Vector3(0, 1, -10);
+
+        // Moving the camera alongside the player position
+        transform.position = player.transform.position + cameraOffset;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    //Seeting the move value to the pre-defined control scheme (Horizontal) in Unity.
+    public float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
+    }
+
+}
